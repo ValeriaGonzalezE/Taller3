3421a3497228dbfbd813dea9e08b66d8eed02996
unknown <valeriagoes170805@gmail.com>
2023-02-27 21:27:16 -0500

    primer commit

---
+++ Taller3/src/ActividadTres/Principal.java
@@ -0,0 +1,13 @@
+package ActividadTres;
+
+public class Principal {
+public static void main(String[] args) {
+		
+PuntoTres sr = new PuntoTres();
+sr.stringsPuntoTres();
+}
+}
+
+	
+ 
+	
---
+++ Taller3/src/ActividadTres/PuntoTres.java
@@ -0,0 +1,11 @@
+package ActividadTres;
+
+public class PuntoTres {
+	 String palabra1 = "hola ";
+	  String palabra2 = "mundo";
+	  String palabra3 = palabra1 + palabra2 ;
+		public void stringsPuntoTres(){
+			
+	System.out.println(palabra3);
+}
+}
